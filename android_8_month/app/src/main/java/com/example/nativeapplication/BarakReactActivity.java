bcc9508dd1fcaba6abc7e9b06a0a8272659cb9e1
Barak Halevi <barak@msapps.mobi>
2021-01-30 23:14:10 +0200

    replace

--- android_8_month/app/src/main/java/com/example/nativeapplication/BarakReactActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.nativeapplication;
-
-public class BarakReactActivity extends com.facebook.react.ReactActivity {
-
-    @Override
-    protected String getMainComponentName() { return "SAMPLE_PROJECT_NAME"; } // Name of the RN component that was exported by AppRegistry.registercomponent()
-}
